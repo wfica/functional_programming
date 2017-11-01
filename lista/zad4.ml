ed5003fc4a1068be142a0ae52e0ec55835b0fe40
wfica <wojtekfica@gmail.com>
2017-11-01 20:23:55 +0100

    list 4 full

--- lista/zad4.ml
+++ lista/zad4.ml
@@ -23,3 +23,81 @@ wszystkich wartości w drzewie. Zapisz tę funkcję w CPS-ie, a następnie zmody
 funkcję tak by w przypadku napotkania wartości 0 funkcja wykonała bezpośredni skok do miejsca 
 swego wywołania, bez krokowego powracania z rekursji.
 *)
+open Core.Std;;
+
+type 'a btree = Leaf | Node of 'a btree * 'a * 'a btree;;
+
+let rec fact_cps n k = 
+  if n = 0 
+  then k 1 
+  else fact_cps (n-1) (fun v -> k (n*v))
+;;
+
+let rec prod_cps tree = fun k -> 
+  match tree with
+  | Leaf -> k 1
+  | Node(left, v, right) -> 
+    prod_cps left @@
+    fun left_prod -> prod_cps right @@
+    fun right_prog -> k @@ v * left_prod * right_prog
+;;
+
+let prod tree = prod_cps tree (fun x -> x)
+;;
+
+
+exception Zero;;
+
+let rec prod_cps_exn tree = fun k -> 
+  match tree with
+  | Leaf -> k 1
+  | Node(_, 0, _) -> raise Zero
+  | Node(left, v, right) -> 
+    prod_cps left @@
+    fun left_prod -> prod_cps right @@
+    fun right_prog -> k @@ v * left_prod * right_prog
+;;
+
+let prod2 tree =
+  try 
+    prod_cps_exn tree (fun x -> x)
+  with
+  | Zero -> 0
+;;
+
+let test () =
+  let t1 = Node (Leaf, 5, Leaf) in 
+  let t2 = Node ( Node(Leaf, 5, Leaf), 1, Node(Leaf, 7, Node(Leaf, 8, Leaf))) in 
+  let t3 = Node (Node( Node(Leaf, 3, Leaf),2, Leaf), 1, Leaf) in
+  let t4 = Node(t3, 7, t2) in
+  let t5 = Node( t4, 0, t4) in
+  let t6 = Node(t5, 1, t4) in
+  let trees = [t1; t2; t3; t4; t5; t6] in
+  let results = [5; 280; 6; 11760; 0; 0] in
+  try
+    assert( List.for_all2_exn trees results ~f:(fun x y -> prod x = y) ) ;
+    assert( List.for_all2_exn trees results ~f:(fun x y -> prod2 x = y) ) ;
+    true;
+  with 
+  | _ -> false
+;;
+
+
+(*
+let rec add_cps x y k = k @@ x + y 
+
+let rec square_cps x k = k @@ x * x 
+
+let rec pit_cps x y = 
+  fun k -> square_cps x 
+  (fun x_squared -> square_cps y 
+  (fun y_squared -> add_cps x_squared y_squared  k ))
+*)
+
+
+let mult x = fun y -> x * y ;;
+
+
+
+
+
--- lista/zad5.ml
+++ lista/zad5.ml
@@ -8,24 +8,140 @@ zapobiec temu problemowi, możemy rozbić operację modyfikacji struktury na dwi
 znajdowania miejsca w strukturze, w którym mamy wykonać zmianę, oraz przeprowadzenia zmiany w 
 konkretnym miejscu; musimy też znaleźć odpowiednią pośrednią reprezentację dla takich miejsc.
 
-	1. Zdefiniuj typ danych α place jako pośrednią reprezentację dla typu α list.
-	
-	2. Zaimplementuj funkcje findNth : α list -> int -> α place oraz collapse : α place -> 
+1. Zdefiniuj typ danych α place jako pośrednią reprezentację dla typu α list.
+
+2. Zaimplementuj funkcje findNth : α list -> int -> α place oraz collapse : α place -> 
 α list. Pierwsza powinna lokalizować n-te miejsce na liście, druga — zapominać informację o 
 miejscu i zwracać listę na której działamy.
 
-	3. Zaimplementuj funkcje add : α -> α place -> α place oraz del : α place -> α place, 
+3. Zaimplementuj funkcje add : α -> α place -> α place oraz del : α place -> α place, 
 odpowiednio dodającą i usuwającą element w odpowiednim miejscu listy. Funkcje powinny działać 
 w czasie stałym, oraz spełniać następujące równości: 
-		(a) collapse (add 3 (findNth [1;2;4] 2)) = [1;2;3;4]
-		(b) collapse (del (findNth [1;2;4] 2)) = [1;2]
-		(c) del (add x p) = p dla dowolnych x : α i p : α place
-		
-	4. Zaimplementuj funkcje next : α place -> α place oraz prev : α place -> α place, 
+(a) collapse (add 3 (findNth [1;2;4] 2)) = [1;2;3;4]
+(b) collapse (del (findNth [1;2;4] 2)) = [1;2]
+(c) del (add x p) = p dla dowolnych x : α i p : α place
+
+4. Zaimplementuj funkcje next : α place -> α place oraz prev : α place -> α place, 
 służące do nawigacji w strukturze listy.
 
-	Listy nie są jedynym typem danych dla którego możemy definiować struktury pamiętające 
+Listy nie są jedynym typem danych dla którego możemy definiować struktury pamiętające 
 miejsce w którym pracujemy. Zdefiniuj typ danych α btplace będący pośrednią reprezentacją 
 pozwalającą na modyfikacje drzew binarnych z Zadania 2. Napisz 3 funkcje up, left, right : α 
 place -> α place do nawigacji po drzewie.
 *)
+
+type 'a place = Place of 'a list * 'a list;;
+
+let findNth l n =
+  let rec _findNth rev tail n =
+    match n, tail  with 
+    | 0, _ ->  Place(rev, tail)
+		| _, h::t -> _findNth (h::rev) t (n-1)
+		| _, [] -> failwith "n greater than list length"
+  in 
+  _findNth [] l (n+1)
+;;
+
+
+let collapse p =
+	let rec _collapse rev acc =
+	match rev with 
+	| []-> acc
+	| hd::tl -> _collapse tl (hd::acc)
+	in match p with 
+	| Place(rev, tl) -> _collapse rev tl
+;; 
+
+
+let add x (Place(ptr::rev, tail)) = Place(x::rev, ptr::tail);;
+let del p =
+	match p with
+	| Place([_], []) -> failwith "cannot point po no place"
+	| Place(_::tl, []) -> Place(tl, [])
+	| Place(_::tl, h::t) -> Place(h::tl, t)
+	| Place([], _) as p -> p (* UNREACHABLE! *)
+;;
+
+let next p = 
+	match p with
+	| Place(_, []) -> failwith "out of range"
+	| Place(rev, hd::tl ) -> Place(hd::rev, tl)
+;;
+
+let prev p =
+	match p with 
+	| Place(hd::prev::tl, tail) -> Place(prev::tl, hd::tail)
+	| Place([_], _) -> failwith "out of range"
+	| Place([], _) as p -> p (* UNREACHABLE! *) 
+;;
+
+let test () =
+	let p = findNth [1;2;3;4] 3 in 
+	let q = findNth [1;2;3;4] 2 in
+	let r = findNth [1;2;3;4] 0 in
+	try
+		assert( collapse p = [1;2;3;4] ) ;
+		assert( collapse q = [1;2;3;4] ) ;
+		assert( collapse r = [1;2;3;4] ) ;
+		assert( collapse (add 3 (findNth [1;2;4] 2)) = [1;2;3;4] );
+		assert( collapse (del (findNth [1;2;4] 2)) = [1;2] );
+		assert( del (add 100000 p ) = p ) ;
+		assert( del (add 100000 q ) = q ) ;
+		assert( del (add 100000 r ) = r ) ;
+		assert( prev p = q) ;
+		assert( next q = p) ;
+		assert( prev @@ prev @@ prev p = r) ;
+		assert( next @@ next @@ next r = p) ;
+		true
+	with
+	 _ -> false
+;;
+
+type 'a btree = Leaf | Node of 'a btree * 'a * 'a btree;;
+
+                                (*    Up,        tree *)
+type 'a place_btree = PlaceTree of 'a btree * 'a btree * bool list ;;
+
+let create_place_tree tree = 
+		match tree with 
+		| Leaf -> failwith "cannot point to Leaf (create)"
+		| _ -> PlaceTree(Leaf, tree ,[])
+;;
+
+let tree_of_place (PlaceTree(Leaf, node, [])) = node;;
+
+let left (PlaceTree(u, Node(l, v, r), lb)) = 
+	match l with 
+	| Leaf -> failwith "cannot point to no Leaf (left)"
+	| _  -> PlaceTree( Node(u, v, r), l, false::lb)
+;;
+
+let right (PlaceTree(u, Node(l, v, r), lb)) =
+	match r with 
+	| Leaf -> failwith "cannot point po Leaf (right)"
+	| _ -> PlaceTree( Node(l, v, u ), r , true::lb)
+;;
+
+let up (PlaceTree(u, node, lb)) =
+	match u, lb with
+	| Leaf, _ | _, [] -> failwith "cannot point to Leaf (up)"
+	| Node(ul, uv, ur), false::tail -> PlaceTree(ul, Node(node, uv, ur), tail)
+	| Node(ul, uv, ur), true::tail -> PlaceTree(ur, Node(ul, uv, node), tail)
+;;
+
+let t1 = Node (Leaf, 5, Leaf);;
+let t2 = Node ( Node(Leaf, 5, Leaf), 1, Node(Leaf, 7, Node(Leaf, 8, Leaf)));;
+let t3 = Node (Node( Node(Leaf, 3, Leaf),2, Leaf), 1, Leaf) ;;
+let t4 = Node( t2, 10, t3);;
+
+let test_tree () = 
+	try 
+		assert ( tree_of_place @@  up @@ left @@ create_place_tree t3  = t3 );
+		assert ( tree_of_place @@  up @@ up @@ left @@ left @@ create_place_tree t4  = t4 );
+		assert ( tree_of_place @@  up @@ up @@ right @@ left @@ create_place_tree t4  = t4 );
+		true
+	with
+	_ -> false
+;;
+
+

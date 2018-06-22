1440275c9f8413e18125e50693f366cfa8967d6f
wfica <wojtekfica@gmail.com>
2018-06-22 11:05:32 +0200

    project in a new repo

--- projekt/proof_checker/formula_unification.ml
+++
@@ -1,7 +0,0 @@
-module PT = Proof_type
-open PT
-module TU = Term_unification
-open TU
-open Core.Std
-
-type formula_substitution = (string * formula)
--- projekt/proof_checker/tester.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-make proof_checker
-for i in {1..6}
-do
-    ./proof_checker.native tests/in$i results/out$i
-done
